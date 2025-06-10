5da53ae327e1b4044ac9b773cfc3eb241e26bfdb
mrroot5 <mrroot5@protonmail.com>
2025-06-10 22:13:42 +0200

    Fix phoenix not visible from host machine

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ import Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :hello_world_phoenix, HelloWorldPhoenixWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
+  http: [ip: {0, 0, 0, 0}, port: 4002],
   secret_key_base: "2redGsoNUgPmfVJxu2x/6v+5mrqGyDCRaM7xFha3a+MokVxR+k8oCsETocx4TAra",
   server: false
 
